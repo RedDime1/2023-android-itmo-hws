bb01859c9689212fec9e55be70544abe6ad12d09
Kozlov_Dmitrii <368321@niuitmo.ru>
2024-03-30 14:21:14 +0200

    full task5

--- app/src/main/java/ru/ok/itmo/example/MainActivity.kt
+++ app/src/main/java/ru/ok/itmo/example/MainActivity.kt
@@ -1,5 +1,6 @@
 package ru.ok.itmo.example
 
+import android.os.Build
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
@@ -7,20 +8,23 @@ import android.widget.ProgressBar
 import android.widget.RadioButton
 import android.widget.RadioGroup
 import android.widget.TextView
-import android.widget.Toast
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
 import kotlin.concurrent.thread
+import io.reactivex.rxjava3.core.Observable
+import io.reactivex.rxjava3.schedulers.Schedulers
+import java.util.concurrent.TimeUnit
 
 
 class MainActivity : AppCompatActivity(R.layout.activity_main) {
 
     private var gapTime = 1000L
-    private var running = false //чтобы reset могло остановить
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         val btnThRun : Button = findViewById(R.id.startThRunBtn)
-        val btnRxJava : Button = findViewById(R.id.startThRunBtn)
+        val btnRxJava : Button = findViewById(R.id.startRxJavaBtn)
         val btnReset : Button = findViewById(R.id.resetBtn)
         val progressBar : ProgressBar = findViewById(R.id.progressBar)
         val radioGroup : RadioGroup = findViewById(R.id.chooserRadio)
@@ -40,28 +44,51 @@ class MainActivity : AppCompatActivity(R.layout.activity_main) {
             btnThRun.isEnabled = false
             btnRxJava.isEnabled = false
             thread(true) {
-                while (running) {
+                while (progressBar.progress != progressBar.max) {
                     try {
                         Thread.sleep(gapTime)
                         runOnUiThread {
                             progressBar.progress++
                             textProgress.text = progressBar.progress.toString()
                         }
-                        if (progressBar.progress == progressBar.max) running = true
                     } catch (e: InterruptedException) {
                         e.printStackTrace()
                     }
                 }
                 runOnUiThread {
-                    btnThRun.isEnabled = true
-                    btnRxJava.isEnabled = true
+                    btnReset.isEnabled = true
                 }
             }
         }
 
+        fun useRxJava() {
+            btnThRun.isEnabled = false
+            btnRxJava.isEnabled = false
+            Observable.interval(gapTime, TimeUnit.MILLISECONDS).takeWhile{progressBar.progress != progressBar.max}
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread()).subscribe {
+                    progressBar.progress++
+                    textProgress.text = progressBar.progress.toString()
+                    if (progressBar.progress == progressBar.max) {
+                        btnReset.isEnabled = true
+                    }
+                }
+        }
+
         btnThRun.setOnClickListener {
-            running = true
             useThAndRun()
+        }
+
+        btnRxJava.setOnClickListener {
+            useRxJava()
+        }
+
+        btnReset.setOnClickListener {
+            progressBar.progress = 0
+            textProgress.text = progressBar.progress.toString()
+            btnReset.isEnabled = false
+            btnThRun.isEnabled = true
+            btnRxJava.isEnabled = true
         }
 
     }
